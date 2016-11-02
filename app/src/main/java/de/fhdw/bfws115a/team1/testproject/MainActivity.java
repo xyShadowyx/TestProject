3fd84d8bfa264e24dd637876ac4568d7b49e97a3
xyShadowyx <xyShadowyx@googlemail.com>
2016-11-02 11:55:14 +0100

    Initial commit

---
+++ app/src/main/java/de/fhdw/bfws115a/team1/testproject/MainActivity.java
@@ -0,0 +1,13 @@
+package de.fhdw.bfws115a.team1.testproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
